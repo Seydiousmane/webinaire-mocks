6cfdc7a04d09a59ccff7da5517e29380d2bd976c
Seydina Ousmane Diatta <diattaseydi92@gmail.com>
2023-04-25 10:32:07 +0000

    Initial commit

---
+++ monapp/example2.py
@@ -0,0 +1,30 @@
+import requests
+import random
+
+def get_random_pizzas(quantity=3):
+    """Returns a list of pizza names."""
+    if quantity > 50:
+        raise ValueError('Max quantity is 50')
+    products = []
+    payload = {
+        "action": "process",
+        "json": 1,
+        "tagtype_0": "categories",
+        "tag_contains_0": "contains",
+        "tag_0": "pizzas",
+        "page_size": 1000
+    }
+    response = requests.get(
+        'https://fr.openfoodfacts.org/cgi/search.pl',
+        params=payload
+    )
+    if response.status_code == 200:
+        products = response.json()['products']
+
+    return random.sample([
+        product['product_name'] for product in products
+        if 'product_name' in product
+    ], quantity)
+
+if __name__ == "__main__":
+    print(get_random_pizzas())
